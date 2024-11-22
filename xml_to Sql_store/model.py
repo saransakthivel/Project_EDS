4b1fd5a74dfc9345b82bc593bacf97a25c4a1357
saransakthivel <saransakthivel98@gmail.com>
2024-11-22 15:13:21 +0530

    Modified ID to uuid

--- xml_to Sql_store/model.py
+++ xml_to Sql_store/model.py
@@ -1,12 +1,15 @@
 # models.py
 from sqlalchemy import Column, Integer, Float, String, Date, Time, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.mssql import UNIQUEIDENTIFIER
+import uuid
 
 Base = declarative_base()
 
 class EDSdata(Base):
     __tablename__ = 'EDSdata'  # Table name
 
+    id = Column(UNIQUEIDENTIFIER, primary_key=True, default=uuid.uuid4)
     d_name = Column(String, primary_key=True)
     d_value = Column(Float)
     date_time = Column(DateTime)
